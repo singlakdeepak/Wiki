037c2784e946beefb430bf42836ca741cca9b791
Deepak Singla <singlakdeepak5@gmail.com>
2020-07-04 03:35:21 +0800

    HTML not working. PNG uploaded

--- deaddiction_analysis/make_readme.sh
+++ deaddiction_analysis/make_readme.sh
@@ -40,8 +40,8 @@ do
 Pfolder=${NEW_CONT_MASK_FOLDER}/${FOLDERI}/${CONTJ}/thresolded_at_${cont_thr}/P_masks
 Qfolder=${NEW_CONT_MASK_FOLDER}/${FOLDERI}/${CONTJ}/thresolded_at_${cont_thr}/Q_masks
 echo "* **Threshold = ${cont_thr}** applied to the statistics" >> readme.md
-echo -e "\t * For **P-values**: [Cluster Report](${PREPORTNAME}/report_at_thr_${cont_thr}/report.txt), [Average time series](${preHTTPS}/${PROJECTFOLDER}/${Pfolder}/bokeh_timeseries_plot.html)">>readme.md
-echo -e "\t * For **Q-values**: [Cluster Report](${QREPORTNAME}/report_at_thr_${cont_thr}/report.txt), [Average time series](${preHTTPS}/${PROJECTFOLDER}/${Qfolder}/bokeh_timeseries_plot.html)">>readme.md
+echo -e "\t * For **P-values**: [Cluster Report](${PREPORTNAME}/report_at_thr_${cont_thr}/report.txt), [Average time series](${preHTTPS}/${PROJECTFOLDER}/${Pfolder}/bokeh_timeseries_plo.png)">>readme.md
+echo -e "\t * For **Q-values**: [Cluster Report](${QREPORTNAME}/report_at_thr_${cont_thr}/report.txt), [Average time series](${preHTTPS}/${PROJECTFOLDER}/${Qfolder}/bokeh_timeseries_plo.png)">>readme.md
 echo >> readme.md
 
 done
